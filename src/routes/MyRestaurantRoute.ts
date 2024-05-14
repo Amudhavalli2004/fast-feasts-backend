c76c5c144d2cd61ee1b31c9b2ac4c7c012adc37a
Amudhavalli2004 <amudhameenaravi@gmail..com>
2024-05-14 20:37:46 +0530

    adding user profile feature

--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -1,18 +1,19 @@
-// import express from "express";
-// import multer from "multer";
+import express from "express";
+import multer from "multer";
+import MyRestaurantController from "../controllers/MyRestaurantController";
 // import MyRestaurantController from "../controllers/MyRestaurantController";
-// import { jwtCheck, jwtParse } from "../middleware/auth";
-// import { validateMyRestaurantRequest } from "../middleware/validation";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyRestaurantRequest } from "../middleware/validation";
 
-// const router = express.Router();
+const router = express.Router();
 
-// const storage = multer.memoryStorage();
-// const upload = multer({
-//   storage: storage,
-//   limits: {
-//     fileSize: 5 * 1024 * 1024, //5mb
-//   },
-// });
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, //5mb
+  },
+});
 
 // router.get(
 //   "/order",
@@ -28,24 +29,24 @@
 //   MyRestaurantController.updateOrderStatus
 // );
 
-// router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
+router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
 
-// router.post(
-//   "/",
-//   upload.single("imageFile"),
-//   validateMyRestaurantRequest,
-//   jwtCheck,
-//   jwtParse,
-//   MyRestaurantController.createMyRestaurant
-// );
+router.post(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.createMyRestaurant
+);
 
-// router.put(
-//   "/",
-//   upload.single("imageFile"),
-//   validateMyRestaurantRequest,
-//   jwtCheck,
-//   jwtParse,
-//   MyRestaurantController.updateMyRestaurant
-// );
+router.put(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateMyRestaurant
+);
 
-// export default router;
+export default router;
